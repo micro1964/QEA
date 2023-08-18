61e05751c19883675868a33981ce6b2b8fd861ac
Reggy Williams <reggy_williams@hotmail.com>
2023-08-18 15:35:44 +0100

    Adding framework.pdf

--- src/test/java/com/testautomationuniversity/testcases/CanNavigateToWebsite.java
+++ src/test/java/com/testautomationuniversity/testcases/CanNavigateToWebsite.java
@@ -7,6 +7,10 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.testautomationuniversity.pages.GoogleSearchPage;
 import com.testautomationuniversity.pages.TauLandingPage;
 import com.testautomationuniversity.setup.BaseClass;
@@ -19,9 +23,15 @@ public class CanNavigateToWebsite extends BaseClass{
 		}
 	WebDriver driver;
 	
+	ExtentReports extent = new ExtentReports();
+	ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+	
 	@BeforeTest
 	public void setUp() {
 		this.driver = getWebDriver();
+		
+		extent.attachReporter(spark);
+				
 	}
 	
 	
@@ -33,7 +43,16 @@ public class CanNavigateToWebsite extends BaseClass{
 		
 		googleSearchPage.clickRejectCookiesButton();
 		
-		Assert.assertTrue(googleSearchPage.getPageTitle().toLowerCase().contains("google"));
+		if(googleSearchPage.getPageTitle().toLowerCase().contains("google")){
+			String msg = "The page title contains the expected text";
+			Assert.assertTrue(true,msg);
+			extent.createTest("1.1").log(Status.PASS, msg);
+			}
+		else {
+			String msg = "The page title does not contain the expected text";
+			Assert.assertTrue(false,msg);
+			extent.createTest("1.1").log(Status.FAIL, msg);
+			}
 		
 		String textSearchTerm = "Test Automation University";
 		
@@ -47,13 +66,24 @@ public class CanNavigateToWebsite extends BaseClass{
 		
 		tauLandingPage.waitForWebsite();
 		
-		Assert.assertTrue(tauLandingPage.getPageTitle().toLowerCase().contains(textSearchTerm.toLowerCase()));
+		if(tauLandingPage.getPageTitle().toLowerCase().contains(textSearchTerm.toLowerCase())) {
+			String msg = "The page title contains the expected text";
+			Assert.assertTrue(true,msg);
+			extent.createTest("1.2","Page Title Check").log(Status.PASS, msg);
+			}
+		else {
+			String msg = "The page title does not contain the expected text";
+			Assert.assertTrue(false,msg);
+			extent.createTest("1.2","Page Title Check").log(Status.FAIL, msg).fail(MediaEntityBuilder.createScreenCaptureFromPath("img.jpg").build());
+			}
 		
+		extent.createTest("1.3","Verify Failure Step").fail(MediaEntityBuilder.createScreenCaptureFromPath("img.jpg").build());
 		//tauLandingPage.closePage();
 		}
 	
 	@AfterTest
 	public void cleanUp() {
 		driver.quit();
+		extent.flush();
 	}
 }
