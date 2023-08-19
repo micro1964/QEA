ac09a99d999a5d5a1bdc322edd82d593e7197da4
Reggy Williams <reggy_williams@hotmail.com>
2023-08-20 00:38:21 +0100

    Adding more structure to Rest Assured POC

--- src/test/java/com/poc/ThirdScript.java
+++ src/test/java/com/poc/ThirdScript.java
@@ -2,12 +2,16 @@ package com.poc;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.ArrayList;
+
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
+@Listeners(com.testautomationuniversity.web.utilities.TestListener.class)
 public class ThirdScript {
 	/*
 	 * Restful API Testing using RestAssured
@@ -18,6 +22,12 @@ public class ThirdScript {
 	
 	@Test
 	public void restPocFirstTestCase() {
+		
+		ArrayList <Integer> id = new ArrayList<Integer>();
+		ArrayList <String> employee_name = new ArrayList<String>();
+		ArrayList <Integer> employee_salary = new ArrayList<Integer>();
+		ArrayList <Integer> employee_age = new ArrayList<Integer>();
+		
 		String sUrl = "https://dummy.restapiexample.com";
 		String sResource = "/api/v1/employees";
 		int iStatusCode = 200;
@@ -26,15 +36,26 @@ public class ThirdScript {
 		
 		String response = given().when()
 		.get(sResource)
-		.then().assertThat()
+		.then().assertThat().log().all()
 		.statusCode(iStatusCode).extract().response().asString();
 		
 		JsonPath js = new JsonPath(response);
 		String sStatus = js.getString("status");
-		Assert.assertEquals(sStatus,"success");
+		String sMessage = js.getString("message");
 		
+		int iData = js.getInt("data.size()");
+		for(int i=0; i<iData;i++) {
+			id.add(js.getInt("data["+i+"].id"));
+			employee_name.add(js.get("data["+i+"].employee_name"));
+			employee_salary.add(js.getInt("data["+i+"].employee_salary"));
+			employee_age.add(js.getInt("data["+i+"].employee_age"));
+			}
+		
+		Assert.assertEquals(employee_name.size(), iData);
+		Assert.assertEquals(employee_salary.size(), iData);
+		Assert.assertEquals(employee_age.size(), iData);
+		Assert.assertEquals(sStatus,"success");
+		Assert.assertEquals(sMessage, "Successfully! All records has been fetched.");		
 	}
 	
-
-
 }
